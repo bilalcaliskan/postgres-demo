c564144bb796dad64bc6b5318031d5ecfe338c74
= <bilalccaliskan@gmail.com>
2018-12-23 13:13:52 +0300

    exception handling

---
+++ src/main/java/com/bcaliskan/postgresdemo/exception/RestResponseEntityExceptionHandler.java
@@ -0,0 +1,40 @@
+package com.bcaliskan.postgresdemo.exception;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.nio.file.AccessDeniedException;
+
+
+@ControllerAdvice
+public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(value = { IllegalArgumentException.class, IllegalStateException.class })
+    public ResponseEntity<Object> handleConflict(RuntimeException ex,
+                                                 WebRequest request) {
+        final String bodyOfResponse = "This should be application specific";
+        return handleExceptionInternal(ex, bodyOfResponse,
+                new HttpHeaders(), HttpStatus.CONFLICT, request);
+    }
+
+    @ExceptionHandler(value = { ResourceNotFoundException.class, ResourceNotFoundException.class })
+    public ResponseEntity<Object> handleResourceNotFoundException(RuntimeException ex,
+                                                                  WebRequest request) {
+        final String bodyOfResponse = "This should be application specific";
+        return handleExceptionInternal(ex, bodyOfResponse,
+                new HttpHeaders(), HttpStatus.NOT_FOUND, request);
+    }
+
+    @ExceptionHandler(value = { AccessDeniedException.class, AccessDeniedException.class })
+    public ResponseEntity<Object> handleAccessDeniedException(Exception ex,
+                                                              WebRequest request) {
+        return new ResponseEntity<>("Access denied message here",
+                new HttpHeaders(), HttpStatus.FORBIDDEN);
+    }
+
+}
